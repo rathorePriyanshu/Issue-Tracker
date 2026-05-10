24d8ca154082c330d759171da1fe34eb26785533
rathorPriyanshu <rathorepriyansh2003@gmail.com>
2026-05-10 16:20:44 +0530

    Add Markdown preview

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+};
+
+export default config;
