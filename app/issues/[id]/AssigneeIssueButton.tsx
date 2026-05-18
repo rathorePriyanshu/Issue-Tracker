730c3dec43a5b90a15ffa7946f0d56846c32db01
rathorPriyanshu <rathorepriyansh2003@gmail.com>
2026-05-18 11:11:40 +0530

    Fetch data with React Query

--- app/issues/[id]/AssigneeIssueButton.tsx
+++ app/issues/[id]/AssigneeIssueButton.tsx
@@ -1,21 +1,26 @@
 "use client";
 
+import Skeleton from "@/app/components/Skeleton";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 const AssigneeIssueButton = () => {
-  const [users, setUsers] = useState<User[]>();
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get("/api/users");
-      setUsers(data);
-    };
+  if (isLoading) return <Skeleton />;
 
-    fetchUsers();
-  }, []);
+  if (error) return null;
 
   return (
     <Select.Root>
