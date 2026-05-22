6c229ef12e7072d1faa8bcf0b55110603c6db710
rathorPriyanshu <rathorepriyansh2003@gmail.com>
2026-05-22 23:11:03 +0530

    Build the layout of Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
-import Pagination from "./components/Pagination";
+import { prisma } from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
+import IssuesChart from "./IssuesChart";
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import { Status } from "@prisma/client";
 
-export default function Home() {
-  return <Pagination itemCount={100} currentPage={1} pageSize={10} />;
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const close = await prisma.issue.count({
+    where: { status: "CLOSE" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const total = await prisma.issue.count();
+
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5" className="max-h-[100%]">
+        <IssueSummary
+          open={open}
+          close={close}
+          inProgress={inProgress}
+          total={total}
+        />
+        <IssuesChart open={open} close={close} inProgress={inProgress} />
+      </Flex>
+      <Box>
+        <LatestIssues />
+      </Box>
+    </Grid>
+  );
 }
